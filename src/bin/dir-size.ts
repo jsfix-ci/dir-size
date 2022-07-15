53949bbd6debc06e3a31dd33c1fd777f6aa9a851
jsfix.live analysis <info@coana.tech>
2022-07-15 11:35:48 +0200

    JSFIX patches

--- src/bin/dir-size.ts
+++ src/bin/dir-size.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import chalk from 'chalk';
-import program from 'commander';
+import { program } from 'commander';
 import fs from 'fs-extra';
 import ora from 'ora';
 import path from 'path';
